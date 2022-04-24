48ffafb84593c202827a38238c7d80be7eb7b280
phucnx-IT <phucnx.phar@gmail.com>
2022-04-24 19:29:04 +0700

    configure-security

---
+++ ecom-backend/src/main/java/cybersoft/java16/ecom/common/configure/SecurityConfig.java
@@ -0,0 +1,35 @@
+package cybersoft.java16.ecom.common.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Bean
+	private PasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("admin")
+			.password(bCryptPasswordEncoder().encode("12345678"))
+			.authorities("ROLE_ADMIN");
+	}
+
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors();
+		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().anyRequest().permitAll();
+	}
+
+}
--- ecom-backend/src/main/java/cybersoft/java16/ecom/user/controller/Demo.java
+++
@@ -1,5 +0,0 @@
-package cybersoft.java16.ecom.user.controller;
-
-public class Demo {
-
-}
